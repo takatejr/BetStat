973fed7d6f2a16cd89259e0add682bc20071bb1e
aka takatejr aka person <perkadamian@gmail.com>
2020-09-05 23:46:01 +0200

    some clear

--- betstat-front/src/routes/routes.js
+++ betstat-front/src/routes/routes.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import WelcomePage from '../components/WelcomePage';
 import SkillsPage from '../components/SkillsPage';
-import ProjectsPage from '../components/ProjectsPage';
-import ContactPage from '../components/ContactPage';
 import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
@@ -13,8 +11,6 @@ const AppRouter = () => (
       <Switch>
         <Route path='/' component={WelcomePage} exact={true} />
         <Route path='/skills' component={SkillsPage} />
-        <Route path='/projects' component={ProjectsPage} />
-        <Route path='/contact' component={ContactPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
