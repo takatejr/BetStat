ab90cf81625f5fa9c643bb5dfb4a7d45a65d15a9
aka takatejr aka person <perkadamian@gmail.com>
2020-09-15 01:13:24 +0200

    now is 2late, but i have some traces of not null response from ws

--- betstat-backend/server.js
+++ betstat-backend/server.js
@@ -6,8 +6,17 @@ port = 3080;
 
 URL_FS = "https://www.flashscore.co.uk";
 
+const sleep = howManyTime => new Promise(resolve => setTimeout(resolve, howManyTime));
+
+function waitForResponse(howManyTime) {
+    const data1 = new Date().getSeconds();
+    sleep(howManyTime);
+    const data2 = new Date().getSeconds();
+    console.log(data1 < data2 ? console.log(data2-data1 + 's') : console.log(data1-data2 + 's'))
+};
+
 async function scrapeProduct() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
 
   await page.goto(URL_FS);
@@ -139,20 +148,45 @@ async function seasonScore(ID) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(URL_FS + "/match/" + ID + "/#standings");
+  await page.goto(URL_FS + "/match/" + ID + "/#standings")  ;
+  await page.screenshot({path: 'screenshot.png'});  
+  
+  const hehe = await page
+  .waitForSelector('.rows___1vntYow > div.selected___2YWEk7U > span')
+  .then(
 
-  const countTeams = await page.$$eval(`.row___S6WkQ8- + .row___3Gv59rA`, (res) => res.map((el) => console.log(el)));
-  console.log(countTeams)
+  whichOne = await page.$$eval(`.rows___1vntYow > div.selected___2YWEk7U `, (res) => res.map((el) => el.className.includes('.selected___2YWEk7U')? '1': '0'))
 
-  // const [win] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[2]/div[1]/span[2]`);
-  // const [draw] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[2]/div[1]/span[1]`);
-  // const [lose] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[2]/div[1]/span[4]`);
-  // const [goals] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[2]/div[1]/span[5]`);
-  // const [points] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[2]/div[1]/span[6]`);
+  ).then(el => console.log(el));
+console.log(hehe)
 
+
+
+  // const countTeams = await page.$$eval(`.row___S6WkQ8- + .row___3Gv59rA`, (res) => res.map((el) => console.log(el)));
+  // console.log(countTeams)
+  // // + .selected___2YWEk7U 
+  // const whichOne = await page.$$eval(`.rows___1vntYow > div.selected___2YWEk7U `, (res) => res.map((el) => el.className.includes('.selected___2YWEk7U')? '1': '0'));
+  // console.log(whichOne)
+
+  // const works = await page.$$eval(`.rows___1vntYow > div.selected___2YWEk7U `, el => { return el})
+
+  // console.log(works)
+// arx = [];
+
+  // arx.push(whichOne)
+  // for(let i = 0; i < countTeams.length; i++){
+  // const [win] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[${i}]/div[1]/span[2]`);
+  // const [draw] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[${i}]/div[1]/span[3]`);
+  // const [lose] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[${i}]/div[1]/span[4]`);
+  // const [goals] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[${i}]/div[1]/span[5]`);
+  // const [points] = await page.$x(`//*[@id="tournament-table"]/div[3]/div[1]/div/div/div[${i}]/div[1]/span[6]`);
+
+  
+  // }
 
   
   browser.close();
+  // matches.push(arx)
 }
 
 app.get("/", (req, res) => {
